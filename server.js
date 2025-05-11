bd476c459f0b7613bbd94996e7aa8c2194b37b15
Miguel Jacy <katakurimister@gmail.com>
2025-05-10 22:08:33 -0300

    Adiciona backend com Node.js e Node-RED

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const RED = require('node-red');
+
+const app = express();
+const server = http.createServer(app);
+
+const settings = {
+  httpAdminRoot: "/admin",
+  httpNodeRoot: "/ui",
+  userDir: "./.nodered",
+  functionGlobalContext: {},
+  port: process.env.PORT || 3000
+};
+
+RED.init(server, settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(settings.port, () => {
+  console.log("Node-RED rodando na porta", settings.port);
+});
+
+RED.start();
